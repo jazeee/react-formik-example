6b9b17d327c0b48a0f65082236fbe25236d8c4ab
Jaz Singh <jaz@color.com>
2019-10-19 17:14:14 -0700

    Convert to Formik

--- src/auth/SignIn/Form.tsx
+++ src/auth/SignIn/Form.tsx
@@ -1,33 +1,44 @@
 import * as React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  password: Yup.string()
+    .min(6, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 
 interface Props {
   handleSubmit: (email: string, password: string) => void
 }
 
-export default function Form(props: Props) {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    props.handleSubmit(email, password);
-  }
+export default function SignInForm(props: Props) {
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor='email'>Email</label>
-      <input
-        id='email'
-        type='text'
-        value={email}
-        onChange={event => setEmail(event.target.value)}
-      />
-      <label htmlFor='password'>Password</label>
-      <input
-        id='password'
-        type='password'
-        value={password}
-        onChange={event => setPassword(event.target.value)}
-      />
-      <button type='submit'>Sign In!</button>
-    </form>
+    <>
+      <Formik
+        initialValues={{
+          email: '', password: '',
+        }}
+        validationSchema={schema}
+        onSubmit={( values ) => {
+          props.handleSubmit(values.email, values.password);
+        }}
+      >
+        {() => (
+          <Form>
+            <Field type='email' name='email' />
+            <ErrorMessage name='email' component='div' />
+            <Field type='password' name='password' />
+            <ErrorMessage name='password' component='div' />
+            <button type='submit'>Sign In!</button>
+          </Form>
+        )}
+      </Formik>
+    </>
   )
 }
